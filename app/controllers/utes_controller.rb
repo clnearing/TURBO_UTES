f2c85f172fc52d25037951eb35c7781f5ee34546
Chris Nearing <clnearing@gmail.com>
2024-06-03 14:30:34 +0700

    complete utes controller

--- app/controllers/utes_controller.rb
+++ app/controllers/utes_controller.rb
@@ -1,11 +1,22 @@
 class UtesController < ApplicationController
+  before_action :set_ute, only: [:show, :edit, :update, :destroy]
+
   def index
+    @utes = Ute.all
   end
 
   def new
+    @ute = Ute.new
   end
 
   def create
+    @ute = Ute.new(ute_params)
+    @ute.user_id = current_user.id
+    if @ute.save
+      redirect_to ute_path(@ute)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -15,9 +26,25 @@ class UtesController < ApplicationController
   end
 
   def update
+    if @ute.update(ute_params)
+      redirect_to ute_path(@ute)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @ute.destroy
+    redirect_to utes_path
   end
-  
+
+  private
+
+  def ute_params
+    params.require(:ute).permit(:model, :year, :location, :details, :image_url, :price)
+  end
+
+  def set_ute
+    @ute = Ute.find(params[:id])
+  end
 end

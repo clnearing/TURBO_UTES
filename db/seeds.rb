8108995935a414bd8e9aed549a2eb3890de480f8
Jason Wang <jasonwang332@gmail.com>
2024-06-04 14:11:39 +1000

    finished view for new

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "seeding..."
+
+User.create(
+  first_name: "bob",
+  last_name: "sponge",
+  email: "bob@gmail.com",
+  password: "bob123"
+)
+
+puts "user created"

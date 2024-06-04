de65a3a0e7a217dd3bbd8456b9215a181575ccbb
Jason Wang <jasonwang332@gmail.com>
2024-06-04 17:30:10 +1000

    bookings controller added

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,15 +1,41 @@
 class BookingsController < ApplicationController
+  before_action :set_ute, only: %i[new create update]
 
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.ute = @ute
+
+    if @booking.save
+      redirect_to root_path
+    else
+      render :new, status: unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    @booking.ute = @ute
+    if @booking.update(booking_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_ute
+    @ute = Ute.find(params[:ute_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:)
   end
 
 end

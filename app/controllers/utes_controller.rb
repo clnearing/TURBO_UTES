d09ff4d1efcffaa75ae0d22a78a817d8b60797c3
Chris Nearing <clnearing@gmail.com>
2024-06-04 14:19:36 +0700

    devise validations added and created a partial for forms

--- app/controllers/utes_controller.rb
+++ app/controllers/utes_controller.rb
@@ -1,5 +1,6 @@
 class UtesController < ApplicationController
   before_action :set_ute, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @utes = Ute.all

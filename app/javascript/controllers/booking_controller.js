892da5f0a4428c14c35631c859678fd5219303d2
Jason Wang <jasonwang332@gmail.com>
2024-06-05 16:59:22 +1000

    completed booking total price

--- app/javascript/controllers/booking_controller.js
+++ app/javascript/controllers/booking_controller.js
@@ -2,13 +2,37 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="booking"
 export default class extends Controller {
-  static targets = ["totalPrice"]
-
   connect() {
     console.log("Hello, I am connected");
   }
 
   fire() {
-    this.totalPriceTarget.innerText = `$500`;
+    const pricePerDay = document.querySelector(".price").id;
+    const startDates = document.querySelectorAll(".booking_start_date .form-select");
+    const totalPrice = document.querySelector("#total-price");
+    const startDateArray = [];
+    const endDateArray = [];
+
+    startDates.forEach((date) => {
+      const option = date.querySelector("option:checked")
+      startDateArray.push(option.value);
+    })
+
+    const endDates = document.querySelectorAll(".booking_end_date .form-select");
+    endDates.forEach((date) => {
+      const option = date.querySelector("option:checked")
+      endDateArray.push(option.value);
+    })
+
+    const startDate = new Date(
+      `${startDateArray[0]}-${startDateArray[1]}-${startDateArray[2]}`
+    );
+
+    const endDate = new Date(
+      `${endDateArray[0]}-${endDateArray[1]}-${endDateArray[2]}`
+    );
+
+    const daysDiff = (endDate - startDate) / (24 * 60 * 60 * 1000);
+    totalPrice.innerText = `Total Price: ${pricePerDay * daysDiff}`;
   }
 }

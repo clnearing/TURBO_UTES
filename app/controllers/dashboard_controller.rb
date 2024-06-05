fab19ff8c9e4e71384d2cb9b03ae264177bfff38
Murielle Beaulieu <murielle.beaulieu19@gmail.com>
2024-06-05 12:14:25 +1000

    dashboard progress my_utes/my_utes bookings

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,11 +1,17 @@
 class DashboardController < ApplicationController
+# On our dashboard, we want to display the utes we own (My utes),
+# the bookings made on our utes (My Utes Bookings) and the bookings we've made on other utes (Booking Made By Me)
 
   def index
-    # we want to display links for our 3 actions bellow
     @utes = Ute.all
-    @my_utes = Ute.where(user_id: current_user.id) # my utes are the one that have my user id
-    #my_utes_bookings # @bookings  that are made on the utes I created
-    #ookings_made_by_me # @bookings that are created by me
+    # my utes are the one that have my user id
+    @my_utes = Ute.where(user_id: current_user.id)
+
+    # my_utes_bookings - bookings that are made on the utes I created
+    @my_utes_bookings = Booking.where(ute_id: @my_utes) # maybe?
+
+    # bookings_made_by_me - bookings that are created by me
+    @bookings_made_by_me = Booking.where(user_id: current_user.id)
   end
 
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,4 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :ute
-  belongs_to :review
 end

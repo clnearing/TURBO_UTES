ce4f49ddec9eb452845bb921f2e8f6f6ef4fcd22
Chris Nearing <clnearing@gmail.com>
2024-06-06 08:13:43 +0700

    tidy up the output

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :ute
   # belongs_to :review
+  validates :end_date, comparison: { greater_than: :start_date }
 
   enum :status, { pending: 0, declined: 1, accepted: 2 }
 end

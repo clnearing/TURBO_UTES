87d63ea31b4eef8c4cdcee018faea9e3ff73e55d
Murielle Beaulieu <murielle.beaulieu19@gmail.com>
2024-06-06 12:00:11 +1000

    completed index search function

--- app/models/ute.rb
+++ app/models/ute.rb
@@ -1,4 +1,11 @@
 class Ute < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+
+  include PgSearch::Model
+  pg_search_scope :search_by_model_year_and_location,
+  against: [ :model, :year, :location ],
+  using: {
+    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  }
 end

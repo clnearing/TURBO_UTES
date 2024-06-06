11e071474e7e1c1d97fbb5682be486a83fcc5d83
Murielle Beaulieu <murielle.beaulieu19@gmail.com>
2024-06-06 16:04:19 +1000

    first save for dashboard v3

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_06_014949) do
     t.index ["ute_id"], name: "index_bookings_on_ute_id"
   end
 
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false

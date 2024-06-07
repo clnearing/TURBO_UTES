ed1bd5598d67f4e5c043bfbcf518993e094bc269
Chris Nearing <clnearing@gmail.com>
2024-06-07 08:29:28 +0700

    mapbox markers and new seed data

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -7,6 +7,21 @@ export default class extends Controller {
     apiKey: String,
     markers: Array
   }
+  connect() {
+    mapboxgl.accessToken = this.apiKeyValue
+
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: "mapbox://styles/mapbox/streets-v10"
+    })
+    this.#addMarkersToMap()
+    this.#fitMapToMarkers()
+
+    this.map.addControl(new MapboxGeocoder ({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl }));
+  }
+
   #addMarkersToMap() {
     console.log(this.markersValue)
     this.markersValue.forEach((marker) => {
@@ -16,19 +31,13 @@ export default class extends Controller {
     })
   }
 
-
-  connect() {
-    mapboxgl.accessToken = this.apiKeyValue
-
-    this.map = new mapboxgl.Map({
-      container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
-    })
-    this.#addMarkersToMap()
-    this.map.addControl(new MapboxGeocoder ({
-      accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl }));
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
+
+
 
 
 }

6ca81aef3534da22660910f39340dd2501862fa4
Jason Wang <jasonwang332@gmail.com>
2024-06-07 14:35:04 +1000

    add review form done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,21 @@
 class ReviewsController < ApplicationController
   def new
+    @review = Review.new
+  end
 
+  def create
+    @review = Review.new(review_params)
+    @review.booking_id = params[:booking_id]
+    if @review.save
+      redirect_to dashboard_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :comments, :booking_id)
   end
 end
